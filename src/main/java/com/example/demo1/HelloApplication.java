adc38919bd6fcfe666975942303219a985eae238
dylan stroud <dylan@strouds.net>
2023-11-22 20:19:41 +1000

    Initial commit

---
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+        Scene scene = new Scene(root, 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/demo1/LoginController.java
@@ -0,0 +1,84 @@
+package com.example.demo1;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class LoginController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    private static Scanner scan;
+    @FXML
+    private TextField user;
+    @FXML
+    private TextField pwd;
+
+    String username;
+    String password;
+
+    public void switchToMainPage(ActionEvent event) throws IOException {
+
+        username = user.getText();
+        password = pwd.getText();
+
+        if(verifyLogin(username, password, "C:\\Users\\dylan\\JavaProjects\\demo1\\src\\main\\resources\\com\\example\\demo1\\Staff.txt")){
+            root =  FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root, 600 , 600);
+            stage.setScene(scene);
+            stage.show();
+        }
+
+    }
+    public void switchToLoginPage(ActionEvent event) throws IOException {
+
+        root =  FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root, 600 , 600);
+        String css = this.getClass().getResource("Loginpage.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static boolean verifyLogin(String username, String password, String filepath){
+
+        boolean found = false;
+
+        String tempUser = "";
+        String tempPwd = "";
+
+        try{
+            scan = new Scanner(new File(filepath));
+            scan.useDelimiter("[,\n]");
+
+            while(scan.hasNext() && !found){
+                tempUser = scan.next();
+                tempPwd = scan.next();
+                if( tempUser.trim().equals(username.trim()) && tempPwd.trim().equals(password.trim())){
+
+                    found = true;
+                }
+            }
+            if(!found){
+                System.out.println("User not found");
+            }
+            scan.close();
+
+        }
+        catch(Exception e) {
+            System.out.println("path not found");
+        }
+        return found;
+    }
+}
